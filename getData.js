2b7d5db5c957a40181d420753fc75deb7b3bc58a
Leslie Ochoa <7255556+leslienochoa@users.noreply.github.com>
2021-04-30 18:01:19 -0700

    Updates

--- getData.js
+++
@@ -1,9 +0,0 @@
-function init() {
-  Tabletop.init( {
-    key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
-    simpleSheet: true }
-  ).then(function(data, tabletop) { 
-    console.log(data)
-  })
-}
-window.addEventListener('DOMContentLoaded', init)
